2d78fd3f97d91593d2cb0d22e2594b5eabece356
Ran QUAN / 権然 <quan.ran@live.cn>
2014-06-05 16:47:25 +0900

    Add Enum.HasFlag

---
+++ LibGit2Sharp/Core/Compat/EnumExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if !DOT_NET_3_5
+namespace LibGit2Sharp.Core.Compat
+{
+#endif
+    static class EnumExtensions
+    {
+        public static bool HasFlag(this Enum value, Enum test)
+        {
+            if (test == null) return false;
+
+            if (value == null) throw new ArgumentNullException("value");
+
+            if (value.GetType() != test.GetType())
+            {
+                throw new ArgumentException("Enumeration type mismatch.");
+            }
+
+            long num = Convert.ToInt64(test);
+            return ((Convert.ToInt64(value) & num) == num);
+        }
+    }
+#if !DOT_NET_3_5
+}
+#endif
